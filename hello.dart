452ee7de7753407e7b69f1115d4a2f129f09aae8
Iampato <858wpwaweru@gmail.com>
2019-01-04 14:11:25 +0300

    Hello world dart

---
+++ hello.dart
@@ -0,0 +1,15 @@
+void main (){
+  
+  
+
+ //interger
+  int age = 20;
+
+  String name = "Patrick" ;
+
+  double fees_Bal = 12980.98;
+
+  //print ("My name is $name");
+  print ("My name has a length of ${name.length}");
+  //print (fees_Bal * 2);
+}
