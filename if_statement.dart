288e42a377f1dfedc2b649b9c973978389d56216
Iampato <858wpwaweru@gmail.com>
2019-01-05 18:13:07 +0300

    if and else statement

---
+++ if_statement.dart
@@ -0,0 +1,19 @@
+void main(){
+
+int marks = 100;
+if (marks >=70 && marks<=100) {
+  print("A grade!!");
+}
+else if(marks >=50  && marks <70) {
+  print("B grade!!");
+}
+else if (marks >30 && marks <50) {
+  print("C grade!!");
+}
+else if (marks>0 && marks <30) {
+  print("D grade!!");
+}
+else {
+  print ("Invalid Score!!!");
+}
+}
