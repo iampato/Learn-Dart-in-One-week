c8f7ee7f64c7ec72b8f6781947ebe5aa29e43d0b
Iampato <858wpwaweru@gmail.com>
2019-01-06 22:59:56 +0300

    switch statements

---
+++ switch.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  var age =18;
+
+  switch(age){
+    case 18:
+        print("You are too young ");
+        break;
+    case 30:
+         print("Work hard for a promotion ");
+         break;
+    case 45:
+        print("Hope you have started to save");
+        break;
+    case 55:
+        print("You should retire tomorrow");
+        break;
+    default: 
+        print("Sorry wewe ni anestor");
+        break;
+  }
+}
