b5be5500a71a50eb1c2107ba176385156b2258b9
Iampato <858wpwaweru@gmail.com>
2019-01-07 19:01:14 +0300

    break and continue keywords

---
+++ break-continue.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  int i = 0;
+  while (i < 5){
+    i++;
+    
+    if (i == 3){
+      break;
+      
+    }
+    else{
+      continue;
+      print("I will start at ${i}");
+    }
+    
+  }
+}
